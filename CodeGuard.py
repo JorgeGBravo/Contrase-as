2a274c8a095740c7737f4e0cb2d2101324b1394c
JorgeGBravo <jdgbravo@gmail.com>
2020-07-17 22:10:53 +0100

    corregida el tipo de busqueda, insercción de de salida del programa y cambio de algunas variables.

--- CodeGuard.py
+++ CodeGuard.py
@@ -1,4 +1,4 @@
-
+import base64
 import datetime
 import sqlite3
 import string
@@ -18,60 +18,71 @@ def gen():
     ord = input('Programa: ')
     if len(ord) == 0:
         print('Introduce una plataforma')
-        gen()
-    longitud = 18
+        opciones()
+    encript = 18
     valores = string.ascii_letters + string.digits + ord
     cryptogen = SystemRandom()
     pss = ""
-    while longitud > 0:
+    for i in range(18):
         pss = pss + cryptogen.choice(valores)
-        longitud = longitud - 1
-    cur.execute('INSERT OR REPLACE INTO names (serv, psd) VALUES (?, ?)' , (ord , pss))
-    cur.execute('INSERT OR IGNORE INTO pass (date, psd, serv) VALUES (?, ?, ?)' , (d , pss , ord ,))
+        pssbytes = bytes(pss, encoding ="utf-8")
+        for i in range(1, encript + 1):
+            pss64 = base64.b64encode(pssbytes)
+
+    cur.execute('INSERT OR REPLACE INTO names (serv, psd) VALUES (?, ?)' , (ord , pss64))
+    cur.execute('INSERT OR IGNORE INTO pass (date, psd, serv) VALUES (?, ?, ?)' , (d , pss64 , ord ,))
     conn.commit()
     print(pss)
     opciones()
 
 def lectura():
+    decript = 18
     busqueda = input("Escribe tu búsqueda: ")
     if not busqueda:
         print("Búsqueda Inválida")
         opciones()
     sentencia = "SELECT * FROM names WHERE serv LIKE ?;"
-    cur.execute(sentencia , ["%{}%".format(busqueda)])
+    cur.execute(sentencia , ["{}".format(busqueda)])  #'.*pattern.*'
     row = cur.fetchone()
-    ultima = row
-    if ultima == None:
+    if row == None:
         print("No hay Ninguna Coincidencia en la Base de Datos")
         lectura()
+    ultimabytes = row[2]
+    for i in range(1 , decript + 1):
+        ultima = base64.b64decode(ultimabytes)
+    ultima = ultima.decode()
 
     print('                                        PLATAFORMA                    PASSWORD')
     print('')
-    print('Password en uso:                     ' , ultima[:2] , '          ' , ultima[2:])
+    print('Password en uso:                          ', row[1], '                 ', ultima)
     print('---------------------------------------------------------------------------------------')
 
     sentencia2 = "SELECT ALL * FROM pass WHERE serv LIKE ?;"
-    cur.execute(sentencia2 , ["%{}%".format(busqueda)])
-    encuentro = cur.fetchall()
-    psd2 = encuentro
-    if len(psd2) > 1:
+    cur.execute(sentencia2, ["{}".format(busqueda)])
+    datos64 = cur.fetchall()
+    if len(datos64) > 1:
         lista = list()
-        for psd in psd2:
-            lista.append(psd)
-        lista.sort()
-        penultima = lista[len(lista) - 2]
+        for pss64 in datos64:
+            lista.append(pss64)
+            lista.sort()
+            penultima64 = lista[len(lista) - 2]
+            pasw64 = penultima64[2]
+            for i in range(1 , decript + 1):
+                penultima = base64.b64decode(pasw64)
+            penultima = penultima.decode()
 
-        print('Penultima contraseña: ' , penultima)
-        opciones()
+            print('Penultima contraseña:        ', penultima64[1], '        ', penultima)
+            opciones()
     else:
-        print('                      Solo hay un Password en uso editada')
+        print('                      Solo hay un Password Editado')
         opciones()
 
 def opciones():
     print('')
     print('Pulsa: '
           ' A Para Generar -'
-          ' B Para Buscar')
+          ' B Para Buscar -'
+          ' Q Para Salir')
     opcion = input('Elige la opción: ')
     opcion = opcion.lower()
     if len(opcion) < 1:
@@ -79,9 +90,11 @@ def opciones():
         opciones()
     if opcion == 'a':
         gen()
-        return
     if opcion == 'b':
         lectura()
+    if opcion == 'q':
+        print('Hasta Otra')
+        exit()
     else:
         print('Esa Opcion No Existe')
         opciones()
